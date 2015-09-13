b2393f440a4aceee0403bdd3c3cc33aab4a7cd51
j- <jh@live.com.au>
2015-09-13 20:49:27 +1000

    Create State class

--- states.js
+++ states.js
@@ -1,17 +1,56 @@
 /**
- * System state enum. State is one of:
+ * Defines a system state.
+ */
+export class State {
+	/**
+	 * Create a new state. Won't be used outside of this file.
+	 * @param {String} name State name
+	 * @param {Number} value State value
+	 */
+	constructor (name, value) {
+		/**
+		 * Readable state name.
+		 * @type {String}
+		 */
+		this.name = name;
+		/**
+		 * Internal value.
+		 * @type {Number}
+		 */
+		this.value = value;
+	}
+
+	/**
+	 * Get the state's raw value.
+	 * @return {Number} Value
+	 */
+	valueOf () {
+		return this.value;
+	}
+
+
+	/**
+	 * Get the state's readable name.
+	 * @return {String} Name
+	 */
+	toString () {
+		return this.name;
+	}
+}
+
+/**
+ * Map of possible system states. Values are:
  *   - UNKNOWN
  *   - HEALTHY
  *   - WARNING
  *   - DANGER
- * @typedef {Symbol} State
+ * @type {Object<State>}
  */
-
 const STATES = {
-	UNKNOWN: Symbol('unknown'),
-	HEALTHY: Symbol('healthy'),
-	WARNING: Symbol('warning'),
-	DANGER: Symbol('danger'),
+	UNKNOWN: new State('unknown', NaN),
+	HEALTHY: new State('healthy', 2),
+	WARNING: new State('warning', 1),
+	DANGER: new State('danger', 0),
 };
 
 export default STATES;
---
+++ tests/states.js
@@ -0,0 +1,42 @@
+import STATES from '../states';
+import assert from 'assert';
+
+describe('Monitor.STATES', () => {
+	it('should each be defined', () => {
+		assert(STATES.UNKNOWN, 'Unknown state');
+		assert(STATES.HEALTHY, 'Healthy state');
+		assert(STATES.WARNING, 'Warning state');
+		assert(STATES.DANGER, 'Danger state');
+	});
+	it('should each be unique', () => {
+		assert.notEqual(STATES.UNKNOWN, STATES.HEALTHY);
+		assert.notEqual(STATES.UNKNOWN, STATES.WARNING);
+		assert.notEqual(STATES.UNKNOWN, STATES.DANGER);
+		assert.notEqual(STATES.HEALTHY, STATES.WARNING);
+		assert.notEqual(STATES.HEALTHY, STATES.DANGER);
+		assert.notEqual(STATES.WARNING, STATES.DANGER);
+	});
+	it('should each have a string representation', () => {
+		assert.equal(String(STATES.UNKNOWN), 'unknown', 'Unknown state');
+		assert.equal(String(STATES.HEALTHY), 'healthy', 'Healthy state');
+		assert.equal(String(STATES.WARNING), 'warning', 'Warning state');
+		assert.equal(String(STATES.DANGER), 'danger', 'Danger state');
+	});
+	it('should each have a numeric representation (valid states)', () => {
+		assert.equal(Number(STATES.HEALTHY), 2, 'Healthy state');
+		assert.equal(Number(STATES.WARNING), 1, 'Warning state');
+		assert.equal(Number(STATES.DANGER), 0, 'Danger state');
+	});
+	it('should each have a numeric representation (invalid state)', () => {
+		assert(isNaN(Number(STATES.UNKNOWN)), 'Unknown state');
+	});
+	it('should be capable of comparison', () => {
+		let state;
+		state = STATES.WARNING;
+		assert(state < STATES.HEALTHY, 'System is having trouble');
+		state = STATES.HEALTHY;
+		assert(state > STATES.DANGER, 'System is not completely offline');
+		state = STATES.UNKNOWN;
+		assert(!(state >= STATES.WARNING || state <= STATES.WARNING), 'System cannot be compared');
+	});
+});
