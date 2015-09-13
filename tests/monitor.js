69ced8ffd5875d5bcdf48cbbc4e9477399d173b4
j- <jh@live.com.au>
2015-09-13 19:37:26 +1000

    Add further constructor tests

--- tests/monitor.js
+++ tests/monitor.js
@@ -7,5 +7,24 @@ describe('Monitor', () => {
 			const mon = new Monitor();
 			assert(mon instanceof Monitor);
 		});
+		it('should not have any default values', () => {
+			const mon = new Monitor();
+			assert(!mon.lastPing);
+			assert(!mon.warningThreshold);
+			assert(!mon.dangerThreshold);
+		});
+		it('should accept an options object', () => {
+			const lastPing = new Date();
+			const warningThreshold = 1000;
+			const dangerThreshold = 2000;
+			const mon = new Monitor({
+				lastPing,
+				warningThreshold,
+				dangerThreshold,
+			});
+			assert.equal(mon.lastPing, lastPing);
+			assert.equal(mon.warningThreshold, warningThreshold);
+			assert.equal(mon.dangerThreshold, dangerThreshold);
+		});
 	});
 });
